31e95e1772d62714e82d1bdb6311f2009e24c8c2
이재서 <n0wst4ndup@gmail.com>
2026-05-19 12:11:43 +0900

    Feat: ShipBody 구현
    - 체력 관련 필드
    - 데미지 처리

--- Assets/Scripts/Shared/Damageable/ShipBody.cs
+++ Assets/Scripts/Shared/Damageable/ShipBody.cs
@@ -2,9 +2,32 @@ using UnityEngine;
 
 public class ShipBody : MonoBehaviour, IDamageable
 {
+    private float _maxHealth;
+    private float _currentHealth;
+
+    public float MaxHealth => _maxHealth;
+    public float CurrentHealth => _currentHealth;
+
+    public void Init(ShipData data)
+    {
+        _maxHealth = data.Health;
+        _currentHealth = _maxHealth;
+    }
 
     public void OnDamaged(float damage)
     {
-        throw new System.NotImplementedException();
+        if (_currentHealth <= 0f) return;
+
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
